6b6c523933bb3d51dba3a8ef85e1be080ad54648
rasmusrasmusson <rr@varm.io>
2025-04-25 08:56:02 +0800

    a

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++ build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -1,3 +1,3 @@
 set(__QT_DEPLOY_TARGET_Mist_FILE F:/Mist/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/Mist.exe)
 set(__QT_DEPLOY_TARGET_Mist_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Mist_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
+set(__QT_DEPLOY_TARGET_Mist_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
